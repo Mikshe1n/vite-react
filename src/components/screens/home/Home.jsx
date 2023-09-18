c8e5f6f58b765314185ecb58070210ad61ef628a
Mikshe1n <Mishaili4ev@yandex.ru>
2023-09-18 15:37:07 +0300

    two commit

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import styles from './Home.module.css'
 
-
 const App = () => {
   return (
     <div>
-        <h1>Cars</h1>
-        <div>
-         <div className={styles.item}>
+         <div>
             <div 
             className={styles.image} 
                 style={{
-                backgroundImage: 'url(/i.webp)',
+                backgroundImage: 'url(/logo.png)',
             }}
-            />
             
-            <div className={styles.info}>
-                <h2>car1</h2>
-                <p>100 000</p>
-                <button>read more</button>
-            </div>
-            
+            /><h1>Дисклеймер</h1>
          </div>
+         
+        <div className='info'>
+        <h2>Давай так.</h2>
+        <p>Мы старались здесь что-то интересное и по-истине крутое. То что ты прочитаешь и узнаешь дальше - плод нашей фантазии и желания устроить крутое пати.</p>
+        <p>Если у тебя сейчас нет 15 минут чтобы изучить весь сайт - то не выебывайся, пожалуйста, а найди 15 минут и изучи весь сайт. </p>
+        <p>И сейчас я задам тебе один вопрос который ты должен пронести через весь сайт -а готов ли я проебать это событие?</p>
+        <h2>Все, можешь закрывать дисклеймер и потратить свои 15 минут!</h2>
+        </div>
+        <div className='helpf'>
+            <h3>Управление сайтом НАЖАТИЕМ, А НЕ СКРОЛЛОМ, БЛЯТЬ</h3>
         </div>
     </div>
-
   )
 }
 
